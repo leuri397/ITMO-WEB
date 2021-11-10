955097572d4c85705472137e2fb771b397feb838
leuri <leouri2001@gmail.com>
2021-11-10 12:39:23 +0300

    Added load time in footer

---
+++ js/download_speed.js
@@ -0,0 +1,8 @@
+(function () {
+   let timerStart = Date.now();
+   let timerEnd = Date.now();
+   window.onload = function(){timerEnd = Date.now();
+   document.getElementById("LoadTime").textContent = timerEnd - timerStart;
+   };
+  })();
+  
